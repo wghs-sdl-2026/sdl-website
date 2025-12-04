d27d0d5d83f393334f18c98e41aeee8a3b12b1b5
L42749 <leosu0427@gmail.com>
2025-12-04 10:22:45 +0800

    reset main.tsx to default version

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./i18n.ts"; 
-import { App } from "./app/index.tsx";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import "./index.css";
+import { App } from "@/app";
 
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>,
 );
